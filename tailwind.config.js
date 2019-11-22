253cb2054ca3103dbebee70350dcc9cc92f0ca0e
David Daza <03dazal@gmail.com>
2019-11-22 11:53:42 -0500

    add home banners

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,19 @@
 module.exports = {
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'surfie-blue': '#127e89',
+        'mirage': '#1f2b3d',
+        'eden': '#165b56',
+        'ecru-white': '#f4f0e6',
+        'yellow-orange': '#feb53d',
+        'cabaret': '#d84557',
+      },
+      fontFamily: {
+        'beth-ellen': ['"Beth Ellen"', 'cursive'],
+        'barlow-condensed': ['"Barlow Condensed"', 'sans-serif']
+      },
+    }
   },
   variants: {},
   plugins: []
